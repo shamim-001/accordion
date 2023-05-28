1a1bc8d83525561975d10a40c203cf66455430a5
Shamim Ahsan <s.ahsan.ds96@gmail.com>
2023-05-28 12:21:46 +0600

    final version

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
-const acc = document.querySelectorAll(".accordion");
+const acc = document.querySelectorAll('.accordion');
 
-for (let i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    // this.classList.toggle("active");
+for(let i = 0; i < acc.length; i++){
+  acc[i].addEventListener('click',function (){
     const desc = this.nextElementSibling;
-    const allDesc = document.querySelectorAll(".desc");
-    const activeAcc = document.getElementsByClassName("accordion active");
-    // console.log(desc);
-    // console.log(desc.scrollHeight);
-    if (desc.style.maxHeight) {
+    const allDesc = document.querySelectorAll('.desc');
+    const activeAcc = document.getElementsByClassName('accordion active');
+
+    if(desc.style.maxHeight){
       desc.style.maxHeight = null;
-      this.classList.remove("active");
-    } else {
-      // Remove active class from all tabs
-      for (let i = 0; i < activeAcc.length; i++) { 
-        activeAcc[i].classList.remove("active");
+      this.classList.remove('active');
+    }else {
+
+      for(let i = 0; i < activeAcc.length; i++){
+        activeAcc[i].classList.remove('active');
       }
 
-      // Set the maxHeight of all desc to 0
-      for (let i = 0; i < allDesc.length; i++) {
+      for (let i = 0; i < allDesc.length; i++){
         allDesc[i].style.maxHeight = null;
       }
 
-      // open the current tab clicked
-      desc.style.maxHeight = desc.scrollHeight + "px";
-      this.classList.add("active");
+      this.classList.add('active')
+      desc.style.maxHeight = desc.scrollHeight + 'px';
     }
-  });
+  })
 }
